5a202b8d8d39294f947c1139080d482bde253301
Travis Tillotson <travistillotson@traviss-MBP.attlocal.net>
2023-01-30 22:53:29 -0600

    add load averages to header stats

--- lib/htoprb/header.rb
+++ lib/htoprb/header.rb
@@ -10,17 +10,29 @@ module Htoprb
     def initialize(window = Window.instance)
       @height = HEIGHT
       @window = window.win
+      @total_uptime = 'NA'
+      @load_average = 'NA'
     end
 
     def update_stats
+      calculate_uptime
+
       @window.setpos(0, 0)
       @window << "Tasks: #{total_tasks}, #{total_running} running"
       @window.setpos(1, 0)
-      @window << "Uptime: #{uptime}"
+      @window << "Load average: #{@load_average}"
+      @window.setpos(2, 0)
+      @window << "Uptime: #{@total_uptime}"
     end
 
-    def uptime
-      platform.uptime[/(\d+\sday,\s\d+:\d+)/]
+    def calculate_uptime
+      # surely there's a better way...
+      unless /up\s(?<uptime>\d+\sday,\s\d+:\d+)\s*,.*load averages:\s(?<load_avg>\d+\.\d+\s\d+\.\d+\s\d+\.\d+)/ =~ platform.uptime
+        return
+      end
+
+      @total_uptime = uptime
+      @load_average = load_avg
     end
 
     def platform
